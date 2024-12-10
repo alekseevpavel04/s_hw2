7c8cd7cc725d38ceb9646de154ea7534a29d5514
alekseevpavel04 <alekceevpavel@mail.ru>
2024-12-10 19:59:50 +0300

    add support of wav_augs in configs and yaml

--- src/transforms/wav_augs/__init__.py
+++ src/transforms/wav_augs/__init__.py
@@ -1,1 +1,6 @@
 from src.transforms.wav_augs.gain import Gain
+from src.transforms.wav_augs.noise import ColoredNoise, BackGroundNoise
+from src.transforms.wav_augs.shift import Shift, PitchShift
+from src.transforms.wav_augs.filter import BandPassFilter, BandStopFilter, HighPassFilter, LowPassFilter
+
+__all__ = ["Gain", "ColoredNoise", "BackGroundNoise", "Shift", "PitchShift", "BandPassFilter", "BandStopFilter", "HighPassFilter", "LowPassFilter"]
