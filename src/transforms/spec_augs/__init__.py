879c8a02d1f4a86cc2705cc40bab05a31a07eedb
alekseevpavel04 <alekceevpavel@mail.ru>
2024-12-10 20:09:38 +0300

    add spec_augs

---
+++ src/transforms/spec_augs/__init__.py
@@ -0,0 +1,4 @@
+from src.transforms.spec_augs.masking import FrequencyMasking, TimeMasking
+from src.transforms.spec_augs.stretching import TimeStretch
+
+__all__ = ["FrequencyMasking", "TimeMasking", "TimeStretch"]
