a7ce200cb6f8b2c76d73e918e9b8e442057cdb19
alekseevpavel04 <alekceevpavel@mail.ru>
2024-12-16 21:28:36 +0300

    add sh to download the data

---
+++ download_all_we_need.sh
@@ -0,0 +1,9 @@
+echo 'downloading trained models'
+gdown https://drive.google.com/uc?id=1q2mOusDeSTl52m95AAdlQc9JChaal_JJ
+gdown https://drive.google.com/uc?id=12zfmndtFStrUQdNqWBWUgaXb4SKuumYl
+gdown https://drive.google.com/uc?id=1AN7VFY1L7kpCqW4_bcmYXS4h_8U06x74
+echo 'downloading language models'
+gdown https://drive.google.com/uc?id=17dihC5x66fSvLns3Dh9wXF6yvblL3jUc
+gdown https://drive.google.com/uc?id=1Z-UWrz2szCgPIiCkyCzlgoPDGE2XWTj5
+echo 'downloading vocab'
+gdown https://drive.google.com/uc?id=1g8_Oyja6uUSKT2u68rVijutqBICV9xyQ
